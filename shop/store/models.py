3e453b29032e8ea8305e3451e088061ad2d50834
slavamorozov <morozikchetkiu2@gmail.com>
2024-07-08 23:23:02 +0300

    добавил все

--- shop/store/models.py
+++ shop/store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Goods(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True, null=False)
+    id = models.IntegerField(primary_key=True, unique=True, null=False, max_length=1000)
     name = models.CharField(max_length=100, null=False)
     categoryId = models.IntegerField(null=False, default='1')
     price = models.DecimalField(max_length=12, null=False, max_digits=10, decimal_places=2)
